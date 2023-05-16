e37a093ad652c34acceed8d6e2fb16da297b6f59
error4071 <sergey_tsoy87@mail.ru>
2023-05-17 00:25:42 +0500

    add app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] arguments) {
-        System.out.println("Hello, World!");
-    }
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+
+public final class App {
+
+        @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
+        private String filePath1;
+        @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
+        private String filePath2;
+        @Option(names = {"-f", "--format"}, paramLabel = "format",
+                description = "output format: [default: stylish]", defaultValue = "stylish")
+        private String format;
+        @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+        boolean usageHelpRequested;
+        @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+        boolean versionInfoRequested;
+
+        public static void main(String[] args) {
+                int exitCode = new CommandLine(new App()).execute(args);
+                System.exit(exitCode);
+        }
 }
