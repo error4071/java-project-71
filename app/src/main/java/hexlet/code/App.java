37d5fecdd0afd25fc6ca64719e95a2e9f6743080
error4071 <sergey_tsoy87@mail.ru>
2023-05-24 23:59:30 +0500

    app add

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,29 +1,45 @@
 package hexlet.code;
 
+import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 
-public final class App {
+public final class App implements Callable<Integer> {
+        private static final int SUCCESS_EXIT_CODE = 0;
+        private static final int ERROR_EXIT_CODE = 1;
 
-        @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
-        private String filePath1;
-        @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
-        private String filePath2;
-        @Option(names = {"-f", "--format"}, paramLabel = "format",
-                description = "output format: [default: stylish]", defaultValue = "stylish")
-        private String format;
-        @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-        boolean usageHelpRequested;
-        @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-        boolean versionInfoRequested;
 
-        public static void main(String[] args) {
-                int exitCode = new CommandLine(new App()).execute(args);
-                System.exit(exitCode);
+                @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
+                private String filePath1;
+                @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
+                private String filePath2;
+                @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format: [default: stylish]", defaultValue = "stylish")
+                private String format;
+                @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+                boolean usageHelpRequested;
+                @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+                boolean versionInfoRequested;
+
+                        public static void main(String[] args) {
+                                int exitCode = new CommandLine(new App()).execute(args);
+                                System.exit(exitCode);
+                        }
+
+                        @Override
+                        public Integer call() throws Exception{
+                                try {
+                                        String formattedDiff = Differ.generate(filePath1, filePath2);
+                                        System.out.println(formattedDiff);
+                                } catch (Exception e) {
+                                        System.err.println(e.getMessage());
+                                        return ERROR_EXIT_CODE;
+                                }
+                                return SUCCESS_EXIT_CODE;
+                }
         }
-}
+
+
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,21 +2,42 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-import static picocli.CommandLine.Help.Ansi.Style.parse;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Differ {
-    public static Map getData(String content) throws Exception {
+    public static String generate(String filePath1, String filePath2) throws Exception {
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        Path getFile1 = Paths.get(filePath1);
+        Path getFile1Location = getFile1.toAbsolutePath().normalize();
+        String content1 = Files.readString(getFile1Location);
+        ObjectMapper objectMapper1 = new ObjectMapper();
+        Map<String, Object> file1 = objectMapper1.readValue(content1, new TypeReference<>(){});
 
-        String file1 = "{\"host\":\"hexlet.io\", \"timeout\":\"50\", \"proxy\":\"123.234.53.22\", \"follow\":\"false\"}";
-        Map<String, Object> map
-                = objectMapper.readValue(file1, new TypeReference<Map<String, Object>>(){});
-        String file2 = "{\"timeout\":\"20\", \"verbose\":\"true\", \"host\":\"hexlet.io\"}";
-        Map<String, Object> map2
-                = objectMapper.readValue(file2, new TypeReference<Map<String, Object>>(){});
+        Path getFile2 = Paths.get(filePath2);
+        Path getFile2Location = getFile2.toAbsolutePath().normalize();
+        String content2 = Files.readString((getFile2Location));
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        Map<String, Object> file2 = objectMapper2.readValue(content2, new TypeReference<>(){});
 
-        return null;
+        Set<String> keyFile = new TreeSet<>(file1.keySet());
+        keyFile.addAll(file2.keySet());
+
+        String result = "{\n";
+        for(String key: keyFile) {
+            if (file1.containsKey(key) && file2.containsKey(key) && file1.get(key).equals(file2.get(key))) {
+                result += String.format("    %s: %s\n", key, file1.get(key));
+//            } else if () {
+//                result += String.format("    %s: %s\n", key, );
+//            } else if () {
+//                result += String.format("    %s: %s\n", key, );
+//            } else () {
+//                result += String.format("    %s: %s\n", key, );
+            }
+        }
+        return result;
     }
 }
