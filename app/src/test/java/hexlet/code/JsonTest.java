5388d78946635031783524f17e9d591ab4b760a9
error4071 <sergey_tsoy87@mail.ru>
2023-06-12 00:30:30 +0500

    update test

--- app/src/test/java/hexlet/code/JsonTest.java
+++ app/src/test/java/hexlet/code/JsonTest.java
@@ -1,17 +1,23 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class JsonTest {
+    private static String File1;
+    private static String File2;
+    private static String correctResult;
+    @BeforeAll
+    public void beforeAll() throws Exception {
+        File1 = "./src/test/resources/file1.json";
+        File2 = "./src/test/resources/file2.json";
+        correctResult = Files.readString(Paths.get("./src/test/resources/correctResult"));
+    }
     @Test
     public void jsonTest() throws Exception {
-        String File1 = "./src/test/resources/file1.json";
-        String File2 = "./src/test/resources/file2.json";
-        String correctResult = Files.readString(Paths.get("./src/test/resources/correctResult"));
-
         String result = Differ.generate(File1, File2);
         assertThat(result).isEqualTo(correctResult);
     }
--- app/src/test/java/hexlet/code/YMLTest.java
+++ app/src/test/java/hexlet/code/YMLTest.java
@@ -1,16 +1,24 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class YMLTest {
+
+    private static String File1yml;
+    private static String File2yml;
+    private static String correctResult;
+    @BeforeAll
+    public void beforeAll() throws Exception {
+        File1yml = "./src/test/resources/file1.yml";
+        File2yml = "./src/test/resources/file2.yml";
+        correctResult = Files.readString(Paths.get("./src/test/resources/correctResult"));
+    }
     @Test
     public void ymlTest() throws Exception {
-        String File1yml = "./src/test/resources/file1.yml";
-        String File2yml = "./src/test/resources/file2.yml";
-        String correctResult = Files.readString(Paths.get("./src/test/resources/correctResult"));
 
         String result = Differ.generate(File1yml, File2yml);
         assertThat(result).isEqualTo(correctResult);
